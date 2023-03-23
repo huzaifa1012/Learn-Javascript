3841c195e8caf2e137c5f9e75766bb6f0feea036
huzaifahux <www.pakteach@gmail.com>
2023-03-23 23:30:52 +0500

    some map method changes

---
+++ map , forEach/app.js
@@ -0,0 +1,23 @@
+// ForEach
+// let a = [2, 3, 3, 5, 5, 8, 4, 4, 9]
+// a.forEach((value) => {
+//     console.log(value)
+// })
+
+// .map()
+// let a = [2, 3, 3, 5, 5, 8, 4, 4, 9]
+// a.map((value) => {
+//     console.log(value * 2)
+// })
+// transforming an array of arrays with of their sums
+// let x = [[6, 9, 34], [52, 5, 83], [2, 10, 5]];
+// let sum = x.map(value => value.reduce((pair1, pair2) => pair1 + pair2))
+// console.log(sum)
+
+// .includes()
+// here I'm checking is variable a includes a number "43", and storing the result in a new
+// variable called 'result'. It always gives respone in (true) or (flase)
+
+// let a = [2, 3, 3, 5, 5, 8, 4, 4, 9]
+// let result = a.includes(43)
+// console.log(result)
